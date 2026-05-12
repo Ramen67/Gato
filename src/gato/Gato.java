4209b6603c3abc3bfe9b0ce364a0caa2eb9d4ab1
Emiliano Ortega Monzon <136416077+Ramen67@users.noreply.github.com>
2026-05-12 15:38:30 -0600

    commit i

--- src/gato/Gato.java
+++ src/gato/Gato.java
@@ -15,6 +15,9 @@ public class Gato {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int i = 0;
+        i++;
+        System.out.println(i);
     }
     
 }
